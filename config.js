2168cf004de97680a25572bfbb465586cb2759fa
Per I. H <you@example.com>
2020-11-02 10:58:56 +0100

    Jeppes overide

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+    databaseURI: 'mongodb+srv://paaerAdmin:p3M3LwuUa9AqnmYf@cluster0.ervmq.mongodb.net/Jokes_DB?retryWrites=true&w=majority', // or MongoDB Atlas connection URI
+    localPort: 8080
+};
+
+module.exports = config;
